62f5350ed20e0ef6ce39712120abe26a4cf0eb1f
Fadhil <fadhilsaheer@gmail.com>
2023-12-24 23:15:56 +0530

    feat: echo

--- core-utils/src/bin/echo.rs
+++ core-utils/src/bin/echo.rs
@@ -1,3 +1,10 @@
-fn main() {
+use std::env;
 
+fn main() {
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0); // first argument will be always the program name
+
+    for arg in args {
+        print!("{} ", arg);
+    }
 }
